04710d1e219a3a4884d9d155313081fd5c2e7249
damasio34 <damasio34@gmail.com>
2015-10-26 20:43:51 -0200

    Inclusão de registros pelo seed

--- entity-codefirst-mysql/Migrations/Configuration.cs
+++ entity-codefirst-mysql/Migrations/Configuration.cs
@@ -18,13 +18,13 @@ namespace entity_codefirst_mysql.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Contato.AddOrUpdate(
+              p => p.Nome,
+              new Contato { Nome = "Andrew Peters", Email = "andrewpeters@damasio34.com" },
+              new Contato { Nome = "Brice Lambson", Email = "bricelambson@damasio34.com" },
+              new Contato { Nome = "Rowan Miller", Email = "rowanmiller@damasio34.com" }
+            );
+            
         }
     }
 }
